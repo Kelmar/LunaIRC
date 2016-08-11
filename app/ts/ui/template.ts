7bc06d635e364c963bc0f2261df5361b210881af
Kelmar <kfiresun@gmail.com>
2016-08-11 13:56:51 -0500

    Basic templating system implemented Plan to replace Knockout.js with something a bit lighter in weight.

---
+++ app/ts/ui/template.ts
@@ -0,0 +1,45 @@
+/* ===================================================================== */
+/*
+ * LunaIRC
+ * 
+ * Copyright (c) 2016 by Bryce Simonds
+ */
+/* ===================================================================== */
+
+/**
+ * Simple template engine.
+ * 
+ * Uses the ECMA Script 5 template litteral to perform templating.
+ * 
+ * This is not only super simple, but also rediculously fast! :)
+ */
+export default class Template
+{
+    private m_precompiled: () => string;
+    private m_format: string;
+
+    constructor()
+    {
+        this.m_format = "";
+        this.compile();
+    }
+
+    private compile(): void
+    {
+        this.m_precompiled = eval('(function () { return `' + this.m_format + '`; })');
+    }
+
+    public get format(): string { return this.m_format; }
+    public set format(value: string)
+    {
+        this.m_format = value;
+        this.compile();
+    }
+
+    public render(model: any): string
+    {
+        return this.m_precompiled.call(model);
+    }
+}
+
+/* ===================================================================== */
