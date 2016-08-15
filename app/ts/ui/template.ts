2653cb3954cc2a26743f8570cb88528160243eb8
Kelmar <kfiresun@gmail.com>
2016-08-15 17:04:09 -0500

    Template can now be loaded from various sources.

--- app/ts/ui/template.ts
+++ app/ts/ui/template.ts
@@ -6,6 +6,12 @@
  */
 /* ===================================================================== */
 
+import * as url from 'url';
+import * as fs from 'fs';
+
+import * as jQuery from "jquery";
+
+/* ===================================================================== */
 /**
  * Simple template engine.
  * 
@@ -39,6 +45,51 @@ export default class Template
     public render(model: any): string
     {
         return this.m_precompiled.call(model);
+    }
+
+    /**
+     * Generates a Template object, with a preset format.
+     */
+    public static fromString(format: string): Template
+    {
+        var rval = new Template();
+
+        rval.format = format;
+
+        return rval;
+    }
+
+    /**
+     * Generates a Template object using a DOM element for formatting.
+     */
+    public static fromDom(selector: string): Template
+    {
+        var $element = $(selector);
+
+        if ($element.length < 0)
+            throw `Element not found ${selector}`;
+
+        return Template.fromString($element.html());
+    }
+
+    /**
+     * Generates a Template object with the contents of a file for the format.
+     */
+    public static fromFile(path: string): Template
+    {
+        var html: string;
+        
+        try
+        {
+            html = fs.readFileSync(path, "utf-8");
+        }
+        catch (e)
+        {
+            var errMsg = `Unable to read template file '${path}': ${e}'`;
+            throw errMsg;
+        }
+
+        return Template.fromString(html);
     }
 }
 
